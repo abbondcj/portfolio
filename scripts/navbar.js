fa4102037bfe296db6a780dea138adec6a5f635e
Chris Abbondandelo <abbondcj@gmail.com>
2022-01-13 16:54:25 -0600

    fixed navbar dropdown menu styling, added hover effects to menu items

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,13 +3,13 @@
 export const navbar = () => {
     let body = document.getElementById("nav")
     body.innerHTML = `
-        <nav class="navbar navbar-expand-sm bg-transparent fixed-top">
+        <nav class="navbar navbar-expand-sm bg-transparent">
             <a class="navbar-brand" href="./index.html"><img src="./assets/images/initials-logo.png"></a>
-            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#myNav">
+            <button class="navbar-toggler custom-toggler" data-bs-toggle="collapse" data-bs-target="#myNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="navbar-collapse collapse" id="myNav">
-                <ul class="navbar-nav mx-auto">
+                <ul class="navbar-nav">
                     <li class="nav-item"><a class="nav-link" href="./index.html">About</a></li>
                     <li class="nav-item"><a class="nav-link" href="./projects.html">Projects</a></li>
                     <li class="nav-item"><a class="nav-link" href="./resume.html">Resume</a></li>
@@ -18,3 +18,11 @@ export const navbar = () => {
             </div>
         </nav>`
 };
+
+
+
+
+
+
+
+
